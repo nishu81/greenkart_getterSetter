8c633e751e6f1b25f1ffff0394dc01494cc4986b
nishu81 <sheikh.s.rahman@gmail.com>
2020-05-14 13:59:50 -0700

    How to handle more than 1 data

--- src/test/java/Page/CartPage.java
+++ src/test/java/Page/CartPage.java
@@ -1,9 +1,11 @@
 package Page;
 
+import Configs.OrderConfiguration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,22 +15,25 @@ public class CartPage {
     private WebDriver driver;
 
     public CartPage(WebDriver driver) {
+
         this.driver = driver;
     }
 
-    public HashMap<String, String> getCartPageData() {
-
+    public HashMap<String, String> getCartPageNameQuantityData() {
+        //ArrayList<OrderConfiguration> lst = new ArrayList<>();
+        // ArrayList<OrderConfiguration> lst
         HashMap<String, String> cartData = new HashMap<String, String>();
-        List<WebElement> colHeader = driver.findElements(By.xpath("//table//td[position()>1]"));
-        List<WebElement> colValues = driver.findElements(By.xpath("//table//td[position()>1]/p"));
+        List<WebElement> vegNames = driver.findElements(By.xpath("//table//tr[position()>1]//td[position()>1]/p[@class='product-name']"));
+        List<WebElement> vegQty = driver.findElements(By.xpath("//table//tr[position()>1]//td[position()>1]/p[@class='quantity']"));
 
-        for (int i = 0, j = 0; i < colHeader.size() && j < colValues.size(); i++, j++) {
-            cartData.put(colHeader.get(i).getText(), colValues.get(j).getText());
+        for (int i = 0, j = 0; i < vegNames.size() && j < vegQty.size(); i++, j++) {
+            cartData.put(vegNames.get(i).getText(), vegQty.get(j).getText());
         }
+
         System.out.println();
-        System.out.println("CART DATA ");
+        System.out.println("CART DATA  NAME: QUANTITY ");
         for (Map.Entry<String, String> mm : cartData.entrySet()) {
-            System.out.println(mm.getKey()+ " "+ mm.getValue());
+            System.out.println(mm.getKey() + " " + mm.getValue());
         }
 
         return cartData;
--- src/test/java/Test/CartPageTest.java
+++ src/test/java/Test/CartPageTest.java
@@ -6,42 +6,46 @@ import Configs.OrderConfiguration;
 import Page.CartPage;
 import Page.HomePage;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Set;
 
 public class CartPageTest extends TestBase {
 
     public WebDriver driver;
     private static OrderConfiguration beetrootOrder;
+    private static OrderConfiguration cucumberOrder;
 
+    ArrayList<OrderConfiguration> al = new ArrayList<>();
 
     @BeforeMethod
     public void setup() throws IOException {
-       /* launchBrowser = new LaunchBrowser();
+        launchBrowser = new LaunchBrowser();
         driver = launchBrowser.getSite(Browsers.CHROME);
         driver.get("https://www.rahulshettyacademy.com/seleniumPractise/#/");
-        beetrootOrder = OrderConfiguration.builder().vegName("Beetroot - 1 Kg").vegQty(2).build();*/
-       testHelpers = new TestHelpers();
+        beetrootOrder = OrderConfiguration.builder().vegName("Beetroot - 1 Kg").vegQty(2).build();
+        cucumberOrder = OrderConfiguration.builder().vegName("Cucumber - 1 Kg").vegQty(3).build();
+       /*testHelpers = new TestHelpers();
        driver = testHelpers.launchSite();
-        beetrootOrder = testHelpers.configureSingleItemOrder(driver);
-
+        beetrootOrder = testHelpers.configureSingleItemOrder(driver);*/
     }
 
     @Test
     public void cartPageTest() throws InterruptedException {
         homePage = new HomePage(driver);
         homePage.configureOrder(beetrootOrder);
+        homePage.configureOrder(cucumberOrder);
         homePage.proceedToCheckOut();
         Thread.sleep(10000);
         cartPage = new CartPage(driver);
-        cartPage.getCartPageData();
-        //Set<String> keySets =  cartPage.getCartPageData().keySet();
-        String cartQuantity= cartPage.getCartPageData().get("Quantiry");
-        beetrootOrder.getVegQty();
+        cartPage.getCartPageNameQuantityData();
+        String cartQuantity = cartPage.getCartPageNameQuantityData().get("Beetroot - 1 Kg");
+        Assert.assertEquals(beetrootOrder.getVegQty(),Integer.parseInt(cartQuantity));
     }
 
     @AfterMethod
